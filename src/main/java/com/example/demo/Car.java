3e486c45bad53bf6143b33f24ecbcc5eb0d18494
Grigor P. Penev <Grigor.Penev@verint.com>
2018-05-17 15:34:33 +0300

    Added a Car Java Bean

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Car {
+
+	private String brand;
+	private String model;
+	
+	public Car(String brand, String model) {
+		super();
+		this.brand = brand;
+		this.model = model;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+	
+}
